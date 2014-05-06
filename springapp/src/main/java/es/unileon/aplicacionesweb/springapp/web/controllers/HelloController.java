d0b373f7748ca4b0f746d95ac99ce8d7c36e623d
Revellado <revellado@gmail.com>
2014-05-06 13:53:20 +0200

    Updated step 1

---
+++ springapp/src/main/java/es/unileon/aplicacionesweb/springapp/web/controllers/HelloController.java
@@ -0,0 +1,29 @@
+package es.unileon.aplicacionesweb.springapp.web.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+    protected final Log logger = LogFactory.getLog(getClass());
+
+    @RequestMapping(value="/hello.htm", method = RequestMethod.GET)
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        logger.info("Returning hello view");
+
+        return new ModelAndView("hello");
+    }
+}
